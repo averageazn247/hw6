1fea0e7261fde7082c5fd71f0a2b567fa2cfc7d8
khoa <averageazn1337@gmail.com>
2013-03-30 18:38:23 -0500

    hw6

---
+++ hw6/HW6.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw6;
+
+/**
+ *
+ * @author Khoa
+ */
+public class HW6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GUI gui=new GUI();
+        System.out.print(" length is "+GUI.getlength());
+    }
+}
